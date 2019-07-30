2601be696e4fbc16bd79b8b91275a769438994c0
Easa Ashok <ashessa@deloitte.com>
2019-07-30 10:43:42 +0000

    User Status

--- Rules/WorkOrders/Operations/AutoTimeEntry/PauseEnable.rule.js
+++ Rules/WorkOrders/Operations/AutoTimeEntry/PauseEnable.rule.js
@@ -1,3 +1,16 @@
-export default function PauseEnable(clientAPI) {
-	return true;
+export default function PauseEnable(context) {
+
+	var JobStarted;
+	var Query = "$expand=MobileStatus";
+	// Fetch User status for operation 
+	context.read('/SAPAssetManager/Services/AssetManager.service', context.binding['@odata.readLink'], [], Query).then(
+		mobileStatus => {
+			JobStarted = mobileStatus.getItem(0).MobileStatus.UserStatus;
+		});
+
+	if (JobStarted) {
+		return false;
+	} else {
+		return true;
+	}
 }
