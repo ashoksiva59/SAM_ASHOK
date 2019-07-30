893c1c8c95ab156a8ea6ec5e431b0c17bbc811d7
Easa Ashok <ashessa@deloitte.com>
2019-07-30 15:03:59 +0000

    Version 55

--- Rules/WorkOrders/Operations/AutoTimeEntry/AutoTimeStart.rule.js
+++ Rules/WorkOrders/Operations/AutoTimeEntry/AutoTimeStart.rule.js
@@ -11,6 +11,13 @@ export default function AutoTimeStart(context) {
 			if (JobStartedStatus == 'STRD') {
 				return; // Dont do any thing as of now, will cover this senario when we have multiple segements of time recorded
 			} else {
+
+				let actionBinding = {
+					Operation: '0010',
+					Description: 'MDK Confirmation',
+					OrderID: '4000341',
+				};
+				context.setActionBinding(actionBinding);
 				// Post Blank Confirmation with current system date and time
 				return context.executeAction('/SAPAssetManager/Actions/WorkOrders/Operations/AutoTimeEntry/AutoTimeConfirmation.action');
 			}
