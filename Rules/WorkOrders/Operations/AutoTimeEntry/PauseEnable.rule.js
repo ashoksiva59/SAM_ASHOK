bf1768887732d8f38f55eeac9fadf1f089c27617
Easa Ashok <ashessa@deloitte.com>
2019-07-30 11:27:37 +0000

    Version 51

--- Rules/WorkOrders/Operations/AutoTimeEntry/PauseEnable.rule.js
+++ Rules/WorkOrders/Operations/AutoTimeEntry/PauseEnable.rule.js
@@ -3,14 +3,14 @@ export default function PauseEnable(context) {
 	var JobStarted;
 	var Query = "$expand=MobileStatus";
 	// Fetch User status for operation 
-	context.read('/SAPAssetManager/Services/AssetManager.service', context.binding['@odata.readLink'], [], Query).then(
+	return context.read('/SAPAssetManager/Services/AssetManager.service', context.binding['@odata.readLink'], [], Query).then(
 		mobileStatus => {
 			JobStarted = mobileStatus.getItem(0).MobileStatus.UserStatus;
+			if (JobStarted == 'STRD') {
+				return true;
+			} else {
+				return false;
+			}
 		});
 
-	if (JobStarted) {
-		return false;
-	} else {
-		return true;
-	}
 }
